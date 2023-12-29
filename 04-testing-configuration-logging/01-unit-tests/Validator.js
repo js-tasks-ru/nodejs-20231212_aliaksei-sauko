6cbfdb3ada4fdf099a6daa1679dfb098b0824528
Aliaksei Sauko <alexeysju@gmail.com>
2023-12-29 23:22:07 +0300

    04-01 added validator fix with a tests

--- 04-testing-configuration-logging/01-unit-tests/Validator.js
+++ 04-testing-configuration-logging/01-unit-tests/Validator.js
@@ -13,25 +13,25 @@ module.exports = class Validator {
       const type = typeof value;
 
       if (type !== rules.type) {
-        errors.push({field, error: `expect ${rules.type}, got ${type}`});
+        errors.push({ field, error: `expect ${rules.type}, got ${type}` });
         return errors;
       }
 
       switch (type) {
         case 'string':
           if (value.length < rules.min) {
-            errors.push({field, error: `too short, expect ${rules.min}, got ${value.length}`});
+            errors.push({ field, error: `too short, expect ${rules.min}, got ${value.length}` });
           }
           if (value.length > rules.max) {
-            errors.push({field, error: `too long, expect ${rules.max}, got ${value.length}`});
+            errors.push({ field, error: `too long, expect ${rules.max}, got ${value.length}` });
           }
           break;
         case 'number':
           if (value < rules.min) {
-            errors.push({field, error: `too little, expect ${rules.min}, got ${value}`});
+            errors.push({ field, error: `too little, expect ${rules.min}, got ${value}` });
           }
           if (value > rules.max) {
-            errors.push({field, error: `too big, expect ${rules.min}, got ${value}`});
+            errors.push({ field, error: `too big, expect ${rules.max}, got ${value}` });
           }
           break;
       }
--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -18,5 +18,44 @@ describe('testing-configuration-logging/unit-tests', () => {
       expect(errors[0]).to.have.property('field').and.to.be.equal('name');
       expect(errors[0]).to.have.property('error').and.to.be.equal('too short, expect 10, got 6');
     });
+
+    it('validate - a value is less than the range - error "too little"', () => {
+      // arrange
+      const rules = {
+        age: { type: 'number', min: 21, max: 35 }
+      };
+      const validator = new Validator(rules);
+
+      // act
+      const errors = validator.validate({ age: 20 });
+
+      // assert
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0])
+        .to.have.property('error')
+        .and.to.be.equal('too little, expect 21, got 20');
+    });
+
+    it('validate - a value that exceeds the range - error "too big"', () => {
+      // arrange
+      const rules = {
+        age: { type: 'number', min: 21, max: 35 },
+      };
+      const validator = new Validator(rules);
+      const objToValidate = { age: 36 };
+
+      // act
+      const errors = validator.validate(objToValidate);
+
+      // assert
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0])
+        .to.have.property('error')
+        .and.to.be.equal(
+          `too big, expect ${rules.age.max}, got ${objToValidate.age}`
+        );
+    });
   });
 });
