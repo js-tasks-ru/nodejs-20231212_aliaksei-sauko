f5bfdd6066cf3ff612ce6eff1b01e8e8103d5b03
Aliaksei Sauko <alexeysju@gmail.com>
2024-01-16 18:53:57 +0300

    06-02 added Products controller implementation

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,47 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+const mongoose = require('mongoose');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  const {subcategory} = ctx.query;
+  const { subcategory } = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  const productEntities = await Product.find({ subcategory: { _id: subcategory } }).exec();
+
+  const products = productEntities.map(mapProduct) || [];
+
+  ctx.body = { products: products };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const productEntities = await Product.find({});
+
+  const products = productEntities.map(mapProduct) || [];
+
+  ctx.body = { products: products };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  const productId = ctx.params.id;
 
+  if (!mongoose.isValidObjectId(productId)) {
+    ctx.throw(400);
+
+    return;
+  }
+
+  if (!productId) return next();
+
+  const productEntity = await Product.findById(productId);
+
+  if (!productEntity) {
+    ctx.throw(404);
+
+    return;
+  }
+
+  const product = mapProduct(productEntity);
+
+  ctx.body = { product: product };
+};
