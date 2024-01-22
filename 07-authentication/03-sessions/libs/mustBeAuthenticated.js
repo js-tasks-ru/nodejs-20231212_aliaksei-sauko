f215ffbb40676d4eb7811adda5b9385c6178bbfc
Aliaksei Sauko <alexeysju@gmail.com>
2024-01-22 20:43:05 +0300

    07-03 added authentication implementation

--- 07-authentication/03-sessions/libs/mustBeAuthenticated.js
+++ 07-authentication/03-sessions/libs/mustBeAuthenticated.js
@@ -1,3 +1,33 @@
-module.exports = function mustBeAuthenticated(ctx, next) {
+const Session = require('../models/Session');
+const mapSessionUser = require('../mappers/user');
+
+module.exports = async function mustBeAuthenticated(ctx, next) {
+  const authHeader = ctx.request.get('Authorization');
+  if (!authHeader) {
+    ctx.throw(401, 'Пользователь не залогинен');
+  }
+
+  const authValues = authHeader.split(' ', 2);
+  const authSchema = authValues[0].toLowerCase();
+
+  if (!authSchema.includes('bearer')) {
+    ctx.throw(401, 'Пользователь не залогинен');
+  }
+
+  const token = authValues[1];
+  const session = await Session.findOne({ token: token }).populate('user');
+
+  if (!session) {
+    ctx.throw(401, 'Неверный аутентификационный токен');
+  }
+
+  if (!session.user) {
+    ctx.throw(404, 'Пользователь не найден');
+  }
+
+  await Session.updateOne({ token: token }, { lastVisit: new Date() });
+
+  ctx.user = mapSessionUser(session.user);
+
   return next();
 };
