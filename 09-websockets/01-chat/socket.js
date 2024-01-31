49e16d5f7d89e5a7c4cfbbf7033aa0372846e41a
Aliaksei Sauko <alexeysju@gmail.com>
2024-01-31 23:43:48 +0300

    09-01 added websocket authentication

--- 09-websockets/01-chat/socket.js
+++ 09-websockets/01-chat/socket.js
@@ -3,10 +3,28 @@ const socketIO = require('socket.io');
 const Session = require('./models/Session');
 const Message = require('./models/Message');
 
+const mapUser = require('./mappers/user');
+
 function socket(server) {
   const io = socketIO(server);
 
-  io.use(async function(socket, next) {
+  io.use(async function (socket, next) {
+    const token = socket.handshake.query.token;
+    if (!token) {
+      next(new Error("anonymous sessions are not allowed"));
+
+      return;
+    }
+
+    const session = await Session.findOne({ token }).populate('user');
+    if (!session) {
+      next(new Error("wrong or expired session token"));
+
+      return;
+    }
+
+    socket.user = mapUser(session.user);
+
     next();
   });
 
