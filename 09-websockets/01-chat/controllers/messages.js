e98ba8881e84d76bc1977c7667b682d886d3d195
Aliaksei Sauko <alexeysju@gmail.com>
2024-02-03 21:44:02 +0300

    09-01 added message sorting with test

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -2,7 +2,11 @@ const Message = require('../models/Message');
 const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  const messageEntities = await Message.find({ chat: ctx.user.id }).limit(20);
+  const messageEntities = await Message
+    .find({ chat: ctx.user.id })
+    .sort({ date: 'desc' })
+    .limit(20);
+
   const messages = messageEntities.map(mapMessage);
 
   ctx.body = {
