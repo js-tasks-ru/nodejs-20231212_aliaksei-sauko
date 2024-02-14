30c734ca201b7a84a9a69b497eb284197f2bc732
Aliaksei Sauko <alexeysju@gmail.com>
2024-02-14 19:00:00 +0300

    08-01 added `register user` middleware implementation

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -3,7 +3,24 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+    const userData = ctx.request.body;
 
+    const user = new User(userData);
+    user.verificationToken = uuid();
+    await user.setPassword(userData.password);
+    await user.save();
+
+    await sendMail({
+        template: 'confirmation',
+        locals: { token: user.verificationToken },
+        to: user.email,
+        from: 'info@08-01.ex',
+        subject: 'Подтвердите почту',
+    });
+
+    ctx.body = { status: 'ok' };
+
+    next();
 };
 
 module.exports.confirm = async (ctx, next) => {
