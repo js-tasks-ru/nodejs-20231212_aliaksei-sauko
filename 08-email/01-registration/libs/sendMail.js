078633a5a2af76e24470bc69e5c1f84488c6e8a3
Aliaksei Sauko <alexeysju@gmail.com>
2024-02-14 17:30:00 +0300

    08-01 added address option `from` to the email sender

--- 08-email/01-registration/libs/sendMail.js
+++ 08-email/01-registration/libs/sendMail.js
@@ -30,25 +30,30 @@ transport.use('compile', htmlToText());
 * options.template - имя файла, содержащего шаблон письма
 * options.locals - объект с переменными, которые будут переданы в шаблон
 * options.to - email, на который будет отправлено письмо
+* options.from - email, от которого будет отправлено письмо
 * options.subject - тема письма
 * пример:
 *     await sendMail({
 *       template: 'confirmation',
 *       locals: {token: 'token'},
 *       to: 'user@mail.com',
+*       from: 'info@domain.com',
 *       subject: 'Подтвердите почту',
 *     });
 * */
 module.exports = async function sendMail(options) {
   const html = pug.renderFile(
-      path.join(__dirname, '../templates', options.template) + '.pug',
-      options.locals || {},
+    path.join(__dirname, '../templates', options.template) + '.pug',
+    options.locals || {},
   );
 
   const message = {
     html: juice(html),
     to: {
       address: options.to,
+    },
+    from: {
+      address: options.from,
     },
     subject: options.subject,
   };
