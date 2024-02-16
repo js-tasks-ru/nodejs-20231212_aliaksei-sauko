6d180f9a1d2c65677dafe80d4a67a38540c6bd41
Aliaksei Sauko <alexeysju@gmail.com>
2024-02-17 00:22:34 +0300

    08-02 added an order checkout implementation

--- 08-email/02-order/controllers/orders.js
+++ 08-email/02-order/controllers/orders.js
@@ -1,7 +1,28 @@
 const Order = require('../models/Order');
+const Product = require('../models/Product');
 const sendMail = require('../libs/sendMail');
+const mapOrderConfirmation = require('../mappers/orderConfirmation');
 
 module.exports.checkout = async function checkout(ctx, next) {
+    const order = new Order(ctx.request.body);
+    order.user = ctx.user.id;
+
+    await order.save();
+
+    const product = await Product.findById(order.product).select('title');
+    const emailLocals = mapOrderConfirmation(order, product);
+
+    await sendMail({
+        template: 'order-confirmation',
+        locals: emailLocals,
+        to: ctx.user.email,
+        from: 'info@08-02.ex',
+        subject: 'Order confirmation',
+    });
+
+    ctx.body = { order: order.id.toString() };
+
+    next();
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
